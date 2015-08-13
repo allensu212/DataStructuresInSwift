452ca931c7fd1d828cf0605dd9ef719b43b8ad22
Wei-Lun Su <wsu8@illinois.edu>
2015-08-12 21:21:10 -0400

    initial commit

---
+++ DataStructuresInSwift/BSTree.swift
@@ -0,0 +1,14 @@
+//
+//  BSTree.swift
+//  DataStructuresInSwift
+//
+//  Created by Wei-Lun Su on 8/12/15.
+//  Copyright (c) 2015 allensu. All rights reserved.
+//
+
+import Foundation
+
+class BSTree {
+    
+    
+}
