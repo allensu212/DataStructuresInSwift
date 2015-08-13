8e6a89b21663a5179c83d09ac552aa2c2469b0ac
Wei-Lun Su <wsu8@illinois.edu>
2015-08-12 20:53:03 -0400

    add new node with addLink function

---
+++ DataStructuresInSwift/LinkedList.swift
@@ -0,0 +1,38 @@
+//
+//  LinkedList.swift
+//  DataStructuresInSwift
+//
+//  Created by Wei-Lun Su on 8/12/15.
+//  Copyright (c) 2015 allensu. All rights reserved.
+//
+
+import Foundation
+
+class LinkedList<T: Equatable> {
+    
+    var headNode: LLNode<T> = LLNode<T>()
+    
+    func addLink(key: T) {
+        if headNode.key == nil {
+            headNode.key = key
+            return
+        }
+        
+        var currentNode: LLNode? = headNode
+        while currentNode != nil {
+            
+            if currentNode?.nextNode == nil {
+                var childNode = LLNode<T>()
+                childNode.key = key
+                childNode.previousNode = currentNode
+                currentNode!.nextNode = childNode
+                break
+            }
+            else {
+                currentNode = currentNode?.nextNode
+            }
+        }
+        
+    }
+    
+}
