d28a729b724930999f2b7b419e0af9a054aaf53b
Wei-Lun Su <wsu8@illinois.edu>
2015-08-12 20:41:24 -0400

    initial commit

---
+++ DataStructuresInSwift/MasterViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ViewController.swift
+//  DataStructuresInSwift
+//
+//  Created by Wei-Lun Su on 8/12/15.
+//  Copyright (c) 2015 allensu. All rights reserved.
+//
+
+import UIKit
+
+class MasterViewController: UIViewController {
+}
+
